db9a0c79db4a1b8ddf80571400292769f6a8c510
Cesar <cesar-pimenta@tuta.io>
2018-11-25 20:50:48 -0200

    'upinicial4'

--- newconecta/settings.py
+++ newconecta/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['smb-impacta.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['smb-impacta.herokuapp.com', 'localhost']
 
 
 # Application definition
