c9bc8a52476b50d0f48ae45b23011d0cf6c5e3e7
Cesar <cesar-pimenta@tuta.io>
2018-11-26 11:58:43 -0200

    'upsdedata2'

--- newconecta/settings.py
+++ newconecta/settings.py
@@ -152,11 +152,5 @@ BOOTSTRAP4 = {
     'include_jquery': True,
 }
 
-MIDDLEWARE_CLASSES = (
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
