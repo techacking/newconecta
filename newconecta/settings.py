bcbba84aa59a8e46ac7e92e3a7ed31a6d0bc9939
Cesar <cesar-pimenta@tuta.io>
2018-11-26 11:46:53 -0200

    'upp2'

--- newconecta/settings.py
+++ newconecta/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'agenda.apps.AgendaConfig',
-    'bootstrap4',
     'bootstrapform',
     'bootstrap_datepicker_plus',
     'home',
